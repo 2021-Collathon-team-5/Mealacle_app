9fec784b2d8dd0015a94e54972a00603378400d6
김산하 <radium96@naver.com>
2021-09-27 23:28:01 +0900

    init commit

---
+++ app/src/main/java/com/naca/mealacle/SplashActivity.java
@@ -0,0 +1,28 @@
+package com.naca.mealacle;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+import androidx.annotation.Nullable;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Handler hd = new Handler(Looper.getMainLooper());
+        hd.postDelayed(new SplashHandler(), 3000);
+    }
+
+    private class SplashHandler implements Runnable {
+        public void run() {
+            startActivity(new Intent(getApplication(), MainActivity.class));
+            SplashActivity.this.finish();
+        }
+    }
+}
